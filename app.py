afcaa9e002565e0200004e8b55d3d5280d460f4f
itu-itis21-yigitem21 <yigitem21@itu.edu.tr>
2023-10-22 20:57:58 +0300

    Added a new route for the test page

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    return "<p>Index page!</p>"
+    return "<h1>Database Project's Homepage</h1>"
+
+@app.route("/test")
+def test_page():
+    return "<h1>Test Page</h1>"
+
+if __name__ == "__main__":
+    app.run() # Run the app server on localhost:5000
