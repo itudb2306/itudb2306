281cb7f30799391e34747e2a44cf2690fd055a44
fatih.baskin <fatih.baskin@tokeninc.com>
2023-11-02 15:16:50 +0300

    Completed divisions table.

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from database import db
 from routes.index import index_blueprint
 from routes.tables.people import table_people_blueprint
 from routes.tables.fielding import table_fielding_blueprint
+from routes.tables.divisions import table_divisions_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(index_blueprint, url_prefix='/')
 app.register_blueprint(table_people_blueprint, url_prefix=TABLES_PREFIX)
 app.register_blueprint(table_fielding_blueprint, url_prefix=TABLES_PREFIX)
+app.register_blueprint(table_divisions_blueprint, url_prefix=TABLES_PREFIX)
 
 """
 @app.route("/")
@@ -17,9 +19,12 @@ def index_page():
     return "<h1>Database Project's Homepage</h1>"
 """
 
+
 @app.route("/test")
 def test_page():
     return "<h1>Test Page</h1>"
 
+
 if __name__ == "__main__":
-    app.run(host=HOST, port=PORT, debug=DEBUG) # Run the app server on localhost:5000
+    # Run the app server on localhost:5000
+    app.run(host=HOST, port=PORT, debug=DEBUG)
---
+++ routes/tables/divisions.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, render_template, request
+from database import db, Query
+
+from config import RECORDS_PER_PAGE
+
+TBLNM = 'divisions'
+
+"""
+ATTENTION:
+    This route is not finished yet. It is just an initial template for the team to create the routes for the tables.
+"""
+
+table_divisions_blueprint = Blueprint(TBLNM, __name__)
+
+
+@table_divisions_blueprint.route('/%s' % TBLNM)
+def view_table():
+    """
+    URL: /tables/divisions?p=
+    """
+    # Pagination
+    page = request.args.get('p', 1, type=int)
+    first_record = (page - 1) * RECORDS_PER_PAGE
+
+    # Query building for table
+    """
+    select 
+        l.league,
+        l.active as lgActive,
+        d.division, 
+        d.active as divActive 
+    from divisions d, leagues l
+    where
+        d.lgID=l.lgID;
+    """
+    query = Query().SELECT('l.league, l.active as lgActive, d.division, d.active as divActive').FROM(
+        'divisions d, leagues l').WHERE('d.lgID=l.lgID').LIMIT(first_record, RECORDS_PER_PAGE).BUILD()
+    print(query)
+    result = db.fetchall(query)
+
+    # Query building for total pages
+    """
+    select count(*) from (
+        select 
+            l.league,
+            l.active as lgActive,
+            d.division, 
+            d.active as divActive 
+        from divisions d, leagues l
+        where
+            d.lgID=l.lgID
+        ) as table_1;
+    """
+    total_pages_query = Query().SELECT('COUNT(*)').FROM('(%s) as table1' %
+                                                        query.removesuffix(';')).BUILD()
+    total_pages = db.fetchone(total_pages_query)[0]
+    total_pages = total_pages // RECORDS_PER_PAGE + 1
+    print(total_pages_query)
+
+    data_recordings = []
+    for row in result:
+        data_recordings.append({'league': row[0],
+                                'lgActive': row[1],
+                                'division': row[2],
+                                'divActive': row[3]})
+
+    return render_template('table_divisions.html', data_list=data_recordings, current_page=page, total_pages=total_pages)
