30adbc3905f12befb745c53c818f026815d53c42
itu-itis21-yigitem21 <yigitem21@itu.edu.tr>
2023-11-12 23:18:31 +0300

    Joined fielding table with people and teams

--- routes/tables/fielding.py
+++ routes/tables/fielding.py
@@ -3,19 +3,62 @@ from database import db, Query
 
 from config import RECORDS_PER_PAGE
 
+"""
+ATTENTION:
+    This route is not finished yet. It is just an initial template for the team to create the routes for the tables.
+"""
+
 table_fielding_blueprint = Blueprint('fielding', __name__)
 
 @table_fielding_blueprint.route('/fielding')
-def view_fielding_table():
+def view_table():
     """
     URL: /tables/fielding?p=
     """
     # Pagination
     page = request.args.get('p', 1, type=int)
     first_record = (page - 1) * RECORDS_PER_PAGE
+    
+    # Query building for table
 
-    # Query building for the "fielding" table
-    query = Query().SELECT('*').FROM('fielding').LIMIT(first_record, RECORDS_PER_PAGE).BUILD()
+    """
+    SELECT
+    p.nameFirst AS FirstName,
+    p.nameLast AS LastName,
+    t.name AS TeamName,
+    f.yearID,
+    f.stint,
+    f.lgID,
+    f.POS,
+    f.G,
+    f.GS,
+    f.InnOuts,
+    f.PO,
+    f.A,
+    f.E,
+    f.DP,
+    f.PB,
+    f.WP,
+    f.SB,
+    f.CS,
+    f.ZR
+    FROM
+        fielding f
+    JOIN
+        people p ON f.playerID = p.playerID
+    JOIN
+        teams t ON f.team_id = t.id;
+    """
+
+    # self._FROM += 'JOIN %s ON %s' % (table_name, condition)
+
+    query = Query().SELECT('p.nameFirst AS FirstName, p.nameLast AS LastName, t.name AS TeamName, f.playerID, f.yearID, f.stint, f.teamID, f.team_ID, f.lgID, f.POS, f.G, f.GS, f.InnOuts, f.PO, f.A, f.E, f.DP, f.PB, f.WP, f.SB, f.CS, f.ZR'
+    ).FROM('fielding f ' 
+    ).JOIN('people p', 'f.playerID = p.playerID'
+    ).JOIN('teams t', 'f.team_id = t.id'
+    ).LIMIT(first_record, RECORDS_PER_PAGE
+    ).BUILD()
+    
     print(query)
     result = db.fetchall(query)
 
@@ -28,26 +71,25 @@ def view_fielding_table():
     data_recordings = []
     for row in result:
         data_recordings.append({
-            'ID': row[0],
-            'playerID': row[1],
-            'yearID': row[2],
-            'stint': row[3],
-            'teamID': row[4],
-            'team_ID': row[5],
-            'lgID': row[6],
-            'POS': row[7],
-            'G': row[8],
-            'GS': row[9],
-            'InnOuts': row[10],
-            'PO': row[11],
-            'A': row[12],
-            'E': row[13],
-            'DP': row[14],
-            'PB': row[15],
-            'WP': row[16],
-            'SB': row[17],
-            'CS': row[18],
-            'ZR': row[19],
+            'FirstName': row[0],
+            'LastName': row[1],
+            'TeamName': row[2],
+            'yearID': row[3],
+            'stint': row[4],
+            'lgID': row[5],
+            'POS': row[6],
+            'G': row[7],
+            'GS': row[8],
+            'InnOuts': row[9],
+            'PO': row[10],
+            'A': row[11],
+            'E': row[12],
+            'DP': row[13],
+            'PB': row[14],
+            'WP': row[15],
+            'SB': row[16],
+            'CS': row[17],
+            'ZR': row[18]
         })
 
     return render_template('table_fielding.html', data_list=data_recordings, current_page=page, total_pages=total_pages)
