4cb93309735ac11fb3cbdec42d899e84b4d69c90
fatih.baskin <fatih.baskin@tokeninc.com>
2023-12-08 18:03:25 +0300

    teams parks normalization

--- database/dataset_normalization.sql
+++ database/dataset_normalization.sql
@@ -1,5 +1,3 @@
-select parkname, country, state, city from parks where state is null or city is null;
-
 UPDATE parks
 SET country = 'US',
     state = 'IL',
@@ -21,16 +19,51 @@ WHERE parkname = 'Ameriquest Field';
 UPDATE parks
 SET city = 'Anaheim',
     state = 'CA',
-    country = 'US'
-WHERE parkname = 'Anaheim Stadium';
-
-UPDATE parks
-SET city = 'Anaheim',
-    state = 'CA',
-    country = 'US'
+    country = 'US',
+    parkalias = 'Anaheim Stadium'
 WHERE parkname = 'Angel Stadium';
 
--- anaheim'den 2 tane var, teams'i de güncelle
+update teams set park_ID = 326 where park_ID = 323;
+delete from parks where ID = 323;
+
+update teams set park_ID = 326 where park_ID = 302;
+delete from parks where ID = 302;
+
+update teams set park_ID = 326 where park_ID = 315;
+delete from parks where ID = 315;
+
+UPDATE parks
+SET country = 'US',
+    state = 'PA',
+    city = 'Philadelphia'
+WHERE parkname = 'Association Park I';
+
+UPDATE parks
+SET city = 'Denver',
+    state = 'CO',
+    country = 'US'
+WHERE parkname = 'Athletic Field';
+
+UPDATE parks
+SET country = 'US',
+    state = 'PA',
+    city = 'Philadelphia'
+WHERE parkname = 'Athletic Park I';
+
+UPDATE parks
+SET country = 'US',
+    state = 'PA',
+    city = 'Philadelphia'
+WHERE parkname = 'Athletic Park II';
+
+update teams set park_ID = 175 where park_ID = 297;
+delete from parks where ID = 297;
+
+UPDATE parks
+SET city = 'Detroit',
+    state = 'MI',
+    country = 'US'
+WHERE parkname = 'Briggs Stadium';
 
 UPDATE parks
 SET country = 'US',
@@ -39,32 +72,286 @@ SET country = 'US',
 WHERE parkname = 'Centennial Grounds';
 
 UPDATE parks
-SET country = 'PR',
-    state = 'NULL',
-    city = 'San Juan'
-WHERE parkname = 'Estadio Hiram Bithorn';
+SET city = 'Chicago',
+    state = 'IL',
+    country = 'US'
+WHERE parkname = 'Comiskey Park';
 
 UPDATE parks
-SET country = 'US',
+SET city = 'Chicago',
+    state = 'IL',
+    country = 'US'
+WHERE parkname = 'Comiskey Park II';
+
+update teams set park_ID = 51 where park_ID = 308;
+delete from parks where ID = 308;
+
+update teams set park_ID = 313 where park_ID = 325;
+delete from parks where ID = 325;
+
+UPDATE parks
+SET city = 'Miami',
+    state = 'FL',
+    country = 'US',
+    parkname = 'Hard Rock Stadium',
+    parkalias = 'Joe Robbie Stadium'
+WHERE parkname = 'Joe Robbie Stadium';
+
+update teams set park_ID = 91 where park_ID = 318;
+delete from parks where ID = 318;
+
+update teams set park_ID = 230 where park_ID = 311;
+delete from parks where ID = 311;
+
+update teams set park_ID = 106 where park_ID = 284;
+delete from parks where ID = 284;
+
+update teams set park_ID = 27 where park_ID = 292;
+delete from parks where ID = 292;
+
+update teams set park_ID = 187 where park_ID = 309;
+delete from parks where ID = 309;
+
+update teams set park_ID = 203 where park_ID = 317;
+delete from parks where ID = 317;
+
+update teams set park_ID = 34 where park_ID = 262;
+delete from parks where ID = 262;
+
+update teams set park_ID = 56 where park_ID = 294;
+delete from parks where ID = 294;
+
+update teams set park_ID = 151 where park_ID = 288;
+delete from parks where ID = 288;
+
+update teams set park_ID = 213 where park_ID = 293;
+delete from parks where ID = 293;
+
+update teams set park_ID = 39 where park_ID = 286;
+delete from parks where ID = 286;
+
+update teams set park_ID = 22 where park_ID = 280;
+delete from parks where ID = 280;
+
+update teams set park_ID = 138 where park_ID = 322;
+delete from parks where ID = 322;
+
+update teams set park_ID = 217 where park_ID = 303;
+delete from parks where ID = 303;
+
+UPDATE parks
+SET city = 'Boston',
+    state = 'MA',
+    country = 'US'
+WHERE parkname = 'Fenway Park I';
+
+
+UPDATE parks
+SET city = 'Boston',
+    state = 'MA',
+    country = 'US'
+WHERE parkname = 'Fenway Park II';
+
+UPDATE parks
+SET city = 'Arlington',
+    state = 'TX',
+    country = 'US',
+    parkname = 'Globe Life Park',
+    parkalias = 'Choctaw Stadium'
+WHERE parkname = 'Globe Life Park in Arlington';
+
+UPDATE parks
+SET city = 'Cincinnati',
+    state = 'OH',
+    country = 'US'
+WHERE parkname = 'Great American Ball Park';
+
+UPDATE parks
+SET city = 'Washington D.C.',
     state = 'DC',
-    city = 'Washington D.C.'
-WHERE parkname = 'Nationals Park';
+    country = 'US'
+WHERE parkname = 'Griffith Stadium I';
 
 UPDATE parks
-SET country = 'US',
+SET city = 'Washington D.C.',
+    state = 'DC',
+    country = 'US'
+WHERE parkname = 'Griffith Stadium II';
+
+update teams set park_ID = 134 where park_ID = 310;
+delete from parks where ID = 310;
+
+UPDATE parks
+SET city = 'Boston',
+    state = 'MA',
+    country = 'US'
+WHERE parkname = 'Huntington Avenue Grounds';
+
+UPDATE parks
+SET city = 'San Diego',
     state = 'CA',
-    city = 'Los Angeles'
+    country = 'US',
+    parkalias = 'San Diego Stadium'
+WHERE parkname = 'Jack Murphy Stadium';
+
+update teams set park_ID = 48 where park_ID = 266;
+delete from parks where ID = 266;
+
+update teams set park_ID = 48 where park_ID = 282;
+delete from parks where ID = 282;
+
+UPDATE parks
+SET city = 'Oakland',
+    state = 'CA',
+    country = 'US',
+    parkalias = 'McAfee Coliseum'
+WHERE parkname = 'Oakland Coliseum';
+
+update teams set park_ID = 304 where park_ID = 327;
+delete from parks where ID = 327;
+
+UPDATE parks
+SET city = 'Louisville',
+    state = 'KY',
+    country = 'US'
+WHERE parkname = 'Municipal Stadium I';
+
+UPDATE parks
+SET city = 'Brooklyn',
+    state = 'NY',
+    country = 'US'
+WHERE parkname = 'Municipal Stadium II';
+
+UPDATE parks
+SET city = 'Philadelphia',
+    state = 'PA',
+    country = 'US'
+WHERE parkname = 'National League Park';
+
+UPDATE parks
+SET city = 'Cincinnati',
+    state = 'OH',
+    country = 'US'
+WHERE parkname = 'National League Park II';
+
+UPDATE parks
+SET city = 'Pittsburgh',
+    state = 'PA',
+    country = 'US'
+WHERE parkname = 'Nationals Grounds';
+
+UPDATE parks
+SET city = 'Detroit',
+    state = 'MI',
+    country = 'US'
+WHERE parkname = 'Navin Field';
+
+update teams set park_ID = 304 where park_ID = 330;
+delete from parks where ID = 330;
+
+UPDATE parks
+SET city = 'Pittsburgh',
+    state = 'PA',
+    country = 'US'
 WHERE parkname = 'Olympics Grounds';
 
 UPDATE parks
-SET country = 'US',
-    state = 'MO',
-    city = 'St. Louis'
+SET city = 'San Francisco',
+    state = 'CA',
+    country = 'US'
+WHERE parkname = 'Oracle Park';
+
+UPDATE parks
+SET city = 'Baltimore',
+    state = 'MD',
+    country = 'US',
+    parkalias = 'Oriole Park at Camden Yards'
+WHERE parkname = 'Oriole Park';
+
+UPDATE parks
+SET city = 'San Francisco',
+    state = 'CA',
+    country = 'US'
+WHERE parkname = 'PacBell Park';
+
+update teams set park_ID = 179 where park_ID = 273;
+delete from parks where ID = 273;
+
+UPDATE parks
+SET city = 'New York City',
+    state = 'NY'
+WHERE parkname = 'Polo Grounds I';
+
+update teams set park_ID = 264 where park_ID = 265;
+delete from parks where ID = 265;
+
+UPDATE parks
+SET city = 'New York City',
+    state = 'NY'
+WHERE parkname = 'Polo Grounds II';
+
+update teams set park_ID = 313 where park_ID = 316;
+delete from parks where ID = 316;
+
+update teams set park_ID = 244 where park_ID = 301;
+delete from parks where ID = 301;
+
+UPDATE parks
+SET city = 'St. Louis',
+    state = 'MO'
 WHERE parkname = 'Red Stocking Baseball Park';
 
 UPDATE parks
-SET country = 'US',
-    state = 'NY',
-    city = 'Brooklyn'
-    parkname = 'Union Grounds'
-WHERE parkname = 'Union Grounds (Brooklyn)';
+SET city = 'Queens',
+    state = 'NY'
+WHERE parkname = 'Ridgewood Park';
+
+update teams set park_ID = 332 where park_ID = 324;
+delete from parks where ID = 324;
+
+UPDATE parks
+SET city = 'St. Paul',
+    state = 'MN'
+WHERE parkname = 'Seventh Street Park';
+
+UPDATE parks
+SET city = 'Seattle',
+    state = 'WA'
+WHERE parkname = 'Sicks Stadium';
+
+update teams set park_ID = 331 where park_ID = 314;
+delete from parks where ID = 314;
+
+UPDATE parks
+SET city = 'Washington D.C.',
+    state = 'DC'
+WHERE parkname = 'Swampdoodle Grounds';
+
+update teams set park_ID = 44 where park_ID = 312;
+delete from parks where ID = 312;
+
+update teams set park_ID = 42 where park_ID = 287;
+delete from parks where ID = 287;
+
+update teams set park_ID = 38 where park_ID = 269;
+delete from parks where ID = 269;
+
+update teams set park_ID = 38 where park_ID = 281;
+delete from parks where ID = 281;
+
+update teams set park_ID = 44 where park_ID = 320;
+delete from parks where ID = 320;
+
+update teams set park_ID = 143 where park_ID = 256;
+delete from parks where ID = 256;
+
+update teams set park_ID = 115 where park_ID = 300;
+delete from parks where ID = 300;
+
+update teams set park_ID = 163 where park_ID = 329;
+delete from parks where ID = 329;
+
+UPDATE parks
+SET city = 'St. Louis',
+    state = 'MO'
+WHERE parkname = 'Sportsman\'s Park IV';
