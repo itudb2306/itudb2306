1675d5d4f509ebe39dafd54b8cf2cd99aa9aabdd
itu-itis21-yigitem21 <yigitem21@itu.edu.tr>
2023-12-17 22:37:03 +0300

    Added js file for seaching

---
+++ static/js/script.js
@@ -0,0 +1,36 @@
+// Add an event listener to the combined input area
+document.getElementById('teamSearch').addEventListener('input', function () {
+    var input, filter, select, option, i;
+    input = document.getElementById('teamSearch');
+    filter = input.value.toUpperCase();
+    select = document.getElementById('team_id');
+    option = select.getElementsByTagName('option');
+
+    // Loop through all options in the select element
+    for (i = 0; i < option.length; i++) {
+        // If the team name contains the input value, display the option, otherwise hide it
+        if (option[i].textContent.toUpperCase().indexOf(filter) > -1) {
+            option[i].style.display = '';
+        } else {
+            option[i].style.display = 'none';
+        }
+    }
+});
+
+document.getElementById('playerSearch').addEventListener('input', function () {
+    var input, filter, select, option, i;
+    input = document.getElementById('playerSearch');
+    filter = input.value.toUpperCase();
+    select = document.getElementById('playerID');
+    option = select.getElementsByTagName('option');
+
+    // Loop through all options in the select element
+    for (i = 0; i < option.length; i++) {
+        // If the team name contains the input value, display the option, otherwise hide it
+        if (option[i].textContent.toUpperCase().indexOf(filter) > -1) {
+            option[i].style.display = '';
+        } else {
+            option[i].style.display = 'none';
+        }
+    }
+} );
