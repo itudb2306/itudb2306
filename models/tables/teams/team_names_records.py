e2f0ab4038a908bc4661e8726f12119ee7f7bb85
fatih.baskin <fatih.baskin@tokeninc.com>
2023-12-18 22:54:44 +0300

    homegames table underway

---
+++ models/tables/teams/team_names_records.py
@@ -0,0 +1,50 @@
+class NamesRecord:
+    def __init__(self, ID: int = None, name: str = None):
+        self.ID = ID
+        self.name = name
+
+    def from_list(self, list):
+        self.ID = list[0]
+        self.name = list[1]
+        return self
+
+    def from_dict(self, dict):
+        self.ID = dict.get('ID', None)
+        return self
+
+    def to_list(self):
+        return [self.ID, self.name]
+
+    def to_dict(self):
+        return {
+            'ID': self.ID,
+            'name': self.name,
+        }
+
+
+class NamesRecords:
+    def __init__(self, records: list = None):
+        self.records = records
+
+    def from_list(self, list):
+        self.records = []
+        for row in list:
+            record = NamesRecord().from_list(row)
+            self.records.append(record)
+
+    def from_dict(self, dict):
+        self.records = []
+        for row in dict:
+            self.records.append(NamesRecord().from_dict(row))
+
+    def to_list(self):
+        list = []
+        for record in self.records:
+            list.append(record.to_list())
+        return list
+
+    def to_dict(self):
+        dict = []
+        for record in self.records:
+            dict.append(record.to_dict())
+        return dict
--- routes/tables/teams.py
+++ routes/tables/teams.py
@@ -283,3 +283,89 @@ def update_record(ID=None):
         print('Record updated: ', ID)
 
     return redirect(url_for('teams.view_details', ID=ID))
+
+
+@table_teams_blueprint.route('/teamnames', methods=['GET', 'POST'])
+def view_team_names():
+    """
+    URL: /tables/teams?p=
+    """
+    # Check if the view exists, if not so, create it.
+    checkTeamsEasyViewExists()
+
+   # Pagination
+    page = request.args.get('p', 1, type=int)
+    first_record = (page - 1) * RECORDS_PER_PAGE
+
+    # Rrequest from form
+    request_form = request.form.to_dict()
+
+    # Filter arguments from the previous request:
+    filter_request_from_prev = request.args.get('filter', None, type=str)
+    filter_dict = urllib.parse.parse_qsl(filter_request_from_prev)
+    filter_dict = dict(filter_dict)
+    filter = FilterForm().from_dict(filter_dict)
+    filter_dict = filter.to_dict()
+    logQuery(f"Filter request from previous page: {filter.to_dict()}")
+
+    # Filter arguments from the new request:
+    filter_request_from_form = FilterForm().from_dict(request_form)
+    if not filter_request_from_form.is_empty():
+        filter = filter_request_from_form
+        filter_dict = filter.to_dict()
+        logQuery(f"Filter request from form: {filter.to_dict()}")
+
+    # Construct the filter string
+    filter_string = filter.to_and_string()
+
+    # Filter arguments from the new request:
+    filter = FilterForm().from_dict(request.form)
+    if filter.to_and_string() != '':
+        print(f"Filter request from filter form: {filter.to_dict()}")
+        filter_string = filter.to_and_string()
+        filter_dict = filter.to_dict()
+
+    # Sorting argıuments from the previous request:
+    sort = request.args.get('sort', None, type=str)
+    sort_dict = urllib.parse.parse_qsl(sort)
+    sort_dict = dict(sort_dict)
+    sort = SortForm().from_dict(sort_dict)
+    sort_dict = sort.to_dict()
+    logQuery(f"Sort request from previous page: {sort.to_dict()}")
+
+    # Sorting arguments from the new request:
+    sort_request_from_form = SortForm().from_dict(request_form)
+    if not sort_request_from_form.is_empty():
+        sort = sort_request_from_form
+        sort_dict = sort.to_dict()
+        logQuery(f"Sort request from form: {sort.to_dict()}")
+
+    # Construct the sort string
+    sort_string = sort.to_and_string()
+
+    # Divisions and leagues query
+    query = Query().SELECT('*').FROM('teams_easy').WHERE(filter_string).ORDER_BY(sort_string).LIMIT(first_record,
+                                                                                                    RECORDS_PER_PAGE).BUILD()
+    result = db.fetchall(query)
+
+    # Query building for total pages
+    total_pages_query = Query().SELECT(
+        'COUNT(*)').FROM('teams_easy').WHERE(filter_string).BUILD()
+    total_pages = db.fetchone(total_pages_query)[0]
+    total_pages = total_pages // RECORDS_PER_PAGE + 1
+
+    leagues_list = getLeaguesList()
+    divisions_list = getDivisionsList()
+    teams_list = getTeamNamesList()
+    parks_list = getParksList()
+
+    data = TableRecords()
+    data.from_list(result)
+    logQuery(f"Records length: {len(data.records)}")
+
+    # Encode filter and sort to pass to template as one string
+    filter_encoded = urllib.parse.urlencode(filter_dict)
+    sort_encoded = urllib.parse.urlencode(sort_dict)
+
+    return render_template('table_teams/table_teams.html', data_list=data.records, current_page=page, total_pages=total_pages, leagues_list=leagues_list,
+                           divisions_list=divisions_list, teams_list=teams_list, parks_list=parks_list, filter=filter_encoded, sort=sort_encoded)
