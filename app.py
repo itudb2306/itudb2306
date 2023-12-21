d109e7549d263aec19421f6a132b152379ca7839
fatih.baskin <fatih.baskin@tokeninc.com>
2023-12-21 20:09:03 +0300

    Fixed github merge

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_login import LoginManager
 from models.users.auth.user import get_user
-from config import (HOST, PORT, DEBUG, TABLES_PREFIX, AUTH_PREFIX, SECRET_KEY, WTF_CSRF_SECRET_KEY)
+from config import (HOST, PORT, DEBUG, TABLES_PREFIX,
+                    AUTH_PREFIX, SECRET_KEY, WTF_CSRF_SECRET_KEY)
 from database import db
 from routes.index import index_blueprint
 from routes.tables.people import table_people_blueprint
@@ -10,22 +11,22 @@ from routes.tables.batting import table_batting_blueprint
 from routes.tables.divisions import table_divisions_blueprint
 from routes.tables.parks import table_parks_blueprint
 from routes.tables.teams import table_teams_blueprint
-<<<<<<< HEAD
 from routes.tables.leagues import table_leagues_blueprint
-
 from routes.users.auth import auth_blueprint
-=======
 from routes.tables.homegames import table_homegames_blueprint
->>>>>>> fatih_tables
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['WTF_CSRF_SECRET_KEY'] = WTF_CSRF_SECRET_KEY
 
 login_manager = LoginManager()
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return get_user(user_id)
+
+
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
@@ -36,13 +37,9 @@ app.register_blueprint(table_divisions_blueprint, url_prefix=TABLES_PREFIX)
 app.register_blueprint(table_parks_blueprint, url_prefix=TABLES_PREFIX)
 app.register_blueprint(table_batting_blueprint, url_prefix=TABLES_PREFIX)
 app.register_blueprint(table_teams_blueprint, url_prefix=TABLES_PREFIX)
-<<<<<<< HEAD
 app.register_blueprint(table_leagues_blueprint, url_prefix=TABLES_PREFIX)
-
 app.register_blueprint(auth_blueprint, url_prefix=AUTH_PREFIX)
-=======
 app.register_blueprint(table_homegames_blueprint, url_prefix=TABLES_PREFIX)
->>>>>>> fatih_tables
 
 """
 @app.route("/")
