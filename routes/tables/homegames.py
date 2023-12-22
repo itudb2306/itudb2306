124cb60b237b6e48d959239577a6988e208c1fd8
fatih.baskin <fatih.baskin@tokeninc.com>
2023-12-22 11:55:31 +0300

    Added some code clearity for homegames.py

--- routes/tables/homegames.py
+++ routes/tables/homegames.py
@@ -4,41 +4,8 @@ from database import db, Query
 from config import RECORDS_PER_PAGE
 from models.tables.homegames.records import Records
 from models.tables.homegames.forms import UpdateForm, FilterForm, SortForm
-from routes.tables.teams import getLeaguesList, getParksList, getTeamNamesList
-from utility import logQuery
+from utility import exceptionPage, checkHomegamesViewExists, getLeaguesList, getParksList, getTeamNamesList
 import urllib.parse
-
-"""
-ATTENTION:
-    This route is not finished yet. It is just an initial template for the team to create the routes for the tables.
-"""
-
-
-def checkHomegamesViewExists():
-    if not db.checkTableExists('homegames_easy'):
-        teams_easy_query = """
-        create view homegames_easy as 
-        select 
-            h.ID as ID,
-            h.yearkey as year,
-            l.league as league,
-            tn.name as team_name,
-            p.parkname as park_name,
-            h.games as games,
-            h.openings as openings,
-            h.attendance as attendance,
-            h.spanfirst_date as spanfirst_date,
-            h.spanlast_date as spanlast_date
-            tn.ID as team_ID,
-            l.lgID as lgID,
-            p.ID as park_ID
-        from homegames h
-        left join teamnames tn on tn.ID = h.team_ID
-        left join parks p on p.ID = h.park_ID
-        left join leagues l on h.leaguekey = l.lgID;
-        """
-        db.execute(teams_easy_query, None)
-
 
 table_homegames_blueprint = Blueprint('homegames', __name__)
 
@@ -48,6 +15,8 @@ def view_table():
     """
     URL: /tables/parks?p=
     """
+    checkHomegamesViewExists()
+
     # Pagination
     page = request.args.get('p', 1, type=int)
     first_record = (page - 1) * RECORDS_PER_PAGE
@@ -61,14 +30,14 @@ def view_table():
     filter_dict = dict(filter_dict)
     filter = FilterForm().from_dict(filter_dict)
     filter_dict = filter.to_dict()
-    logQuery(f"Filter request from previous page: {filter.to_dict()}")
+    print(f"Filter request from previous page: {filter.to_dict()}")
 
     # Filter arguments from the new request:
     filter_request_from_form = FilterForm().from_dict(request_form)
     if not filter_request_from_form.is_empty():
         filter = filter_request_from_form
         filter_dict = filter.to_dict()
-        logQuery(f"Filter request from form: {filter.to_dict()}")
+        print(f"Filter request from form: {filter.to_dict()}")
 
     # Construct the filter string
     filter_string = filter.to_and_string()
@@ -86,14 +55,14 @@ def view_table():
     sort_dict = dict(sort_dict)
     sort = SortForm().from_dict(sort_dict)
     sort_dict = sort.to_dict()
-    logQuery(f"Sort request from previous page: {sort.to_dict()}")
+    print(f"Sort request from previous page: {sort.to_dict()}")
 
     # Sorting arguments from the new request:
     sort_request_from_form = SortForm().from_dict(request_form)
     if not sort_request_from_form.is_empty():
         sort = sort_request_from_form
         sort_dict = sort.to_dict()
-        logQuery(f"Sort request from form: {sort.to_dict()}")
+        print(f"Sort request from form: {sort.to_dict()}")
 
     # Construct the sort string
     sort_string = sort.to_and_string()
@@ -101,26 +70,34 @@ def view_table():
     # Query for table
     query = Query().SELECT('*').FROM('homegames_easy').WHERE(filter_string).ORDER_BY(sort_string).LIMIT(first_record,
                                                                                                         RECORDS_PER_PAGE).BUILD()
-    logQuery(query)
-    result = db.fetchall(query)
+    try:
+        result = db.fetchall(query)
+    except Exception as e:
+        return exceptionPage(e)
 
     total_pages_query = Query().SELECT(
         'COUNT(*)').FROM('homegames_easy').WHERE(filter_string).BUILD()
-    total_pages = db.fetchone(total_pages_query)[0]
+    try:
+        total_pages = db.fetchone(total_pages_query)[0]
+    except Exception as e:
+        return exceptionPage(e)
+
     total_pages = total_pages // RECORDS_PER_PAGE + 1
-    logQuery(total_pages_query)
 
     data = Records()
     data.from_list(result)
-    logQuery(f"Records length: {len(data.records)}")
+    print(f"Records length: {len(data.records)}")
 
     # Encode filter and sort to pass to template as one string
     filter_encoded = urllib.parse.urlencode(filter_dict)
     sort_encoded = urllib.parse.urlencode(sort_dict)
 
-    leagues_list = getLeaguesList()
-    teams_list = getTeamNamesList()
-    parks_list = getParksList()
+    try:
+        leagues_list = getLeaguesList()
+        teams_list = getTeamNamesList()
+        parks_list = getParksList()
+    except:
+        return exceptionPage(e)
 
     return render_template('table_homegames/table_homegames.html', data_list=data.records, current_page=page, total_pages=total_pages,
                            leagues_list=leagues_list, teams_list=teams_list, parks_list=parks_list, filter=filter_encoded, sort=sort_encoded)
@@ -136,7 +113,7 @@ def update_record(ID=None):
     if request.method == 'POST' and ID is not None:
         # Get form data
         form = UpdateForm().from_dict(request.form)
-        logQuery(form.to_dict())
+        print(form.to_dict())
 
         # Get column-value pairs to use in SET clause
         col_val_pairs = form.to_dict()
@@ -146,11 +123,13 @@ def update_record(ID=None):
             'ID = %s' % ID).BUILD()
 
         vals_tuple = form.to_tuple()
-        # None values are generating problems for logging
-        logQuery(query % vals_tuple)
 
         # Execute query
-        db.execute(query, vals_tuple)
+        try:
+            db.execute(query, vals_tuple)
+        except Exception as e:
+            return exceptionPage(e)
+
         print('Record updated: ', ID)
 
     return redirect(url_for('homegames.view_table', **other_args))
--- utility.py
+++ utility.py
@@ -193,3 +193,29 @@ def checkTeamsNamesViewExists():
         ) as tn2 on tn1.ID = tn2.ID;
         """
         db.execute(teams_names_count_query, None)
+
+
+def checkHomegamesViewExists():
+    if not db.checkTableExists('homegames_easy'):
+        teams_easy_query = """
+        create view homegames_easy as 
+        select 
+            h.ID as ID,
+            h.yearkey as year,
+            l.league as league,
+            tn.name as team_name,
+            p.parkname as park_name,
+            h.games as games,
+            h.openings as openings,
+            h.attendance as attendance,
+            h.spanfirst_date as spanfirst_date,
+            h.spanlast_date as spanlast_date
+            tn.ID as team_ID,
+            l.lgID as lgID,
+            p.ID as park_ID
+        from homegames h
+        left join teamnames tn on tn.ID = h.team_ID
+        left join parks p on p.ID = h.park_ID
+        left join leagues l on h.leaguekey = l.lgID;
+        """
+        db.execute(teams_easy_query, None)
